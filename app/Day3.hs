0ff6544cb548c584eb2a2cd05d6d70f65f878c07
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2026-09-21 01:00:47 +0300

    day3: refactor

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,50 +1,76 @@
-import Data.List.Split
 import Text.Read (readMaybe)
 import Data.List
 import Data.Maybe
-import Data.Ord (comparing)
 import Control.Monad.State
+
+newtype Battery = Battery { joltageRating :: Integer } deriving (Eq, Ord)
+newtype Bank = Bank { batteries :: [Battery] }
 
 main :: IO ()
 main = do
     banks <- parse <$> readFile "inputs/Day3.txt"
-    let s1 = maxJoltageSum  2 <$> banks
-    let s2 = maxJoltageSum 12 <$> banks
+    let joltSum n = sum . map (maxJoltage n)
+
+    let s1 = joltSum  2 <$> banks
     putStrLn $ "Part 1: " ++ show s1
+
+    let s2 = joltSum 12 <$> banks
     putStrLn $ "Part 2: " ++ show s2
 
-maxJoltageSum :: Int -> [[Integer]] -> Integer
-maxJoltageSum digits banks = sum $ map (maxJoltage (digits)) banks
-
--- Greedy algorithm - begin from the left most available battery,
+-- Given a number of batteries to be chosen and a bank
+-- determine the maximal possible joltage.
+--
+-- Greedy algorithm implementation - begin from the left most available battery,
 -- but also leave enough batteries on the right and choose one with
 -- the largest joltage, do that recursively.
-maxJoltage :: Int -> [Integer] -> Integer
-maxJoltage digits bank = evalState (go digits) bank where
-    go :: Int -> State [Integer] Integer
-    go 1 = chooseBattery 0
-    go digit = do
-        b <- chooseBattery (digit - 1)
-        rem <- go (digit - 1)
-        pure $ b * 10 ^ (digit - 1) + rem
+maxJoltage :: Int -> Bank -> Integer
+maxJoltage n bank = evalState (go (n - 1)) bank where
+    go :: Int -> State Bank Integer
+    go 0 = joltageRating <$> chooseBattery 0
+    go remaining = do
+        battery <- chooseBattery remaining
+        let rating = joltageRating battery
 
-chooseBattery :: Int -> State [Integer] Integer
+        -- Joltage value of the remaining batteries
+        rest <- go (remaining - 1)
+
+        -- Battery joltage ratings are digits of the joltage value
+        pure $ rating * 10 ^ remaining + rest
+
+-- Choosing a battery shrinks the bank such that
+-- only batteries after it could be chosen. Choosing
+-- is also affected by the remaining number of batteries
+-- to be chosen.
+chooseBattery :: Int -> State Bank Battery
 chooseBattery remaining = do
     bank <- get
-    let available = take (length bank - remaining) bank
-    let (skipped, battery) = maxWithIdx available
-    put $ drop (skipped + 1) bank
+    let available = reserve remaining bank
+    let battery = maximalBattery available
+    -- Chosen battery is always within the bank
+    let bank' = fromMaybe undefined $ shrink bank battery
+    put bank'
     pure battery
 
--- It is very important that this method returns the index
--- of the first occurance of the largest element.
-maxWithIdx :: Ord a => [a] -> (Int, a)
-maxWithIdx xs = (i, a) where
-    a = maximum xs
-    i = fromJust $ elemIndex a xs
+-- Return a subset of the bank without the last `remaining` batteries
+reserve :: Int -> Bank -> Bank
+reserve remaining (Bank bs) = Bank $ take n bs
+    where n = length bs - remaining
 
-parse :: String -> Maybe [[Integer]]
+maximalBattery :: Bank -> Battery
+maximalBattery (Bank batteries) = Battery $ maximum $ map joltageRating batteries
+
+shrink :: Bank -> Battery -> Maybe Bank
+shrink bank chosen = do
+    index <- elemIndex chosen (batteries bank)
+    -- Exclude not only batteries to the left
+    -- but also the chosen battery
+    pure $ Bank $ drop (index + 1) (batteries bank)
+
+parse :: String -> Maybe [Bank]
 parse = traverse parseBank . lines
 
-parseBank :: String -> Maybe [Integer]
-parseBank = traverse readMaybe . map pure
+parseBank :: String -> Maybe Bank
+parseBank input = Bank <$> (traverse parseBattery . map pure) input
+
+parseBattery :: String -> Maybe Battery
+parseBattery = (fmap Battery) . readMaybe
